d52012f7bbd875c6fc2cb2f4be40a7fadc3c60f9
Takashi Yamauchi <water.yin.hare.62@gmail.com>
2025-04-29 14:43:07 +0900

    Series #2 initiall commit

---
+++ basic/src/App.java
@@ -0,0 +1,37 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int number;
+        number = 10;
+        System.out.println(number);
+
+        // Sample of different data types
+        double pi = 3.14159;
+        System.out.println("Value of pi: " + pi);
+
+        char letter = 'A';
+        System.out.println("Character: " + letter);
+
+        boolean isJavaFun = true;
+        System.out.println("Is Java fun? " + isJavaFun);
+
+        String message = "Learning Java!";
+        System.out.println("Message: " + message);
+
+        long largeNumber = 123456789L;
+        System.out.println("Large number: " + largeNumber);
+
+        float decimalNumber = 5.75f;
+        System.out.println("Float value: " + decimalNumber);
+
+        // 算術演算子のサンプル
+        int a = 15;
+        int b = 4;
+
+        System.out.println("足し算 (a + b): " + (a + b));
+        System.out.println("引き算 (a - b): " + (a - b));
+        System.out.println("掛け算 (a * b): " + (a * b));
+        System.out.println("割り算 (a / b): " + (a / b));
+        System.out.println("余り (a % b): " + (a % b));
+    }
+}
